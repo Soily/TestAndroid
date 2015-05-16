3ee521aa4664ecccc157e972aab91f2de97f7a0e
basteln <constant@freethinkers-inc.de>
2015-05-16 13:13:52 +0200

    Expand Comment

--- app/src/main/java/com/developers/basteln/testgitandroidapp/MainActivity.java
+++ app/src/main/java/com/developers/basteln/testgitandroidapp/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /* Comment */
+    /* Comment Test */
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
