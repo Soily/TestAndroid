2d3e522fa1af609e2867dff984eb1fa76037f6a8
Patrick <trancefusion@gno.de>
2015-05-16 17:48:11 +0200

    Comment added

--- app/src/main/java/com/developers/basteln/testgitandroidapp/MainActivity.java
+++ app/src/main/java/com/developers/basteln/testgitandroidapp/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true; //raus hier
+        return true; //raus hier // rein hier
     }
 
     @Override
